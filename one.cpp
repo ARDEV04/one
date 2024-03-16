09f3f41a903fd6796df0d660a2d2974e2f25ebae
ARDEV04 <monuranjan251@gmail.com>
2024-03-16 17:26:26 +0530

    changes updated

--- one.cpp
+++ one.cpp
@@ -7,18 +7,7 @@ int main() {
     while(testcase--){
     void mouse()
     {
-    ll p;
-    cin>>p;
-    vector<ll>vec(p);
-    for(ll i=0;i<p;i++) cin>>vec[i];
-    ll ans=0;
-    for(ll i=0;i<p-1;i++){
-        if(vec[i]>vec[i+1]) {
-            ans++;
-            vec[i+1]=max(vec[i+1],vec[i]);
-        }
-    }
-    cout<<ans<<endl;
+    
     }
     }
 }
